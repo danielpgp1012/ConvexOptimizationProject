28962bbce0d59327ec18ad1781fcd7b241680899
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2019-05-28 17:05:00 +0200

    Added IPM

--- Matlab Source Files/Project_Script.m
+++ Matlab Source Files/Project_Script.m
@@ -13,15 +13,29 @@ substratesModel=extractSubNetwork(model,uptakes); %create submodel only
 cReactions=findCarbonRxns(substratesModel,1); %find substrates that contain
 % AT LEAST 1 carbon
 cReactions = findRxnIDs(model, cReactions);
-model.lb(cReactions) = 0; % set consumption to 0
 
-%% 
+%% Sample Optimization with succinate uptake of -10 g/h
 
 biomass_idx = find(contains(model.rxns,"Biomass_Ecoli_core_N(w/GAM)-Nmet2"));
 succinate_idx = find(contains(model.rxns,'EX_succ(e)'));
 oxygen_idx = find(contains(model.rxns,'EX_o2(e)'));
+model.lb(cReactions) = 0; % set consumption of everything to 0
 model.lb(succinate_idx) = -10;
+fprintf("LP solution: \n");
+tic;
 [obj,x] = optimize(model);
+toc
+
+%% Interior point method
+func = @(x) -model.c'*x;
+options = optimoptions('fmincon','Display','iter','Algorithm','sqp','MaxFunctionEvaluations',300);
+S=model.S; % Extracting the 
+S=full(S); % Creating a full matrix from a sparse matrix
+fprintf("IP solution: \n");
+tic;
+optimal=fmincon(func,x,[],[],S,model.b,model.lb,model.ub,[],options); % Solving the problem using IPM
+toc;
+
 
 %% change oxygen and succinate
 succ_uptake = linspace(-30,0,30);
